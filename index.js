c0ca7c065e207e8ef1e0bdb1a42bc5cc708ecdab
Champi <dannaalejandravr03@gmail.com>
2025-03-07 20:30:50 -0500

    Test server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My app");
+import express from 'express';
+
+const APP = express();
+const PORT = 3000;
+
+APP.get('/', (req, res) => {
+  res.send('Hola gente de yutup!');
+});
+
+APP.listen(PORT, () => {
+  console.log(`🟢 Server running on port ${PORT} 🟢`);
+});
